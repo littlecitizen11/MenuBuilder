a879d18d0f7213c97d4a426508bf3e5cb8ce5d1f
LittleCitizen11 <thelittlecitizen@gmail.com>
2020-08-18 16:33:59 +0300

    add the project in the first Ex.

---
+++ MenuBuilder/MenuBuilder/Menu.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MenuBuilder
+{
+    public class Menu<P,T> : Validations
+    {
+        public Dictionary<P, Option<T>> Dict { get; set; }
+        public Validations Vali { get; set; }
+        public Menu()
+        {
+            Dict = new Dictionary<P, Option<T>>();
+        }
+        public Dictionary<P,Option<T>> GetDict()
+        {
+            return Dict;
+        }
+        public bool IsKeyInDict(P key)
+        {
+            Vali.IsValid(key);
+            try
+            {
+                if (!Dict.ContainsKey(key)) throw new KeyNotFoundException();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("{0} Exception caugt" +e.Message);
+                return false;
+            }
+            return Dict.ContainsKey(key);
+        }
+        public Dictionary<P,Option<T>> GetDictByKey(P key)
+        {
+                if (IsKeyInDict(key))
+                {
+                    var d = (Dictionary<P, Option<T>>)Dict.Where(t => t.Key.Equals(key)).Select(t => t);
+                    return d;
+                }
+            return null;
+        }
+        public void SelectOption(P key)
+        {
+            
+            var dict = GetDictByKey(key);
+            if (!(dict is null))
+            {   var d = Dict.Select(t => t.Value.RunFunc);
+                foreach (var item in d)
+                {
+                    item.Run();
+                }
+            }
+        }
+
+
+    }
+}
---
+++ MenuBuilder/MenuBuilder/Option.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MenuBuilder
+{
+    public class Sums : IAction<int>
+    {
+        public void Run(params int[] param)
+        {
+            int sum = 0;
+            foreach (var item in param)
+            {
+                sum += item;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+    public class checkOption
+    {
+        public checkOption()
+        {
+            int[] a = new int[5] { 1, 2, 3, 4, 5 };
+            Sums sums = new Sums();
+            Option<int> p = new Option<int>("blabla",sums);
+        }
+    }
+    public class Option<T>
+    {
+        public string Description { get; set; }
+        public IAction<T> RunFunc { get; set; }
+        public Option(string description, IAction<T> action)
+        {
+            Description = description;
+            RunFunc = action;
+        }
+    }
+}
